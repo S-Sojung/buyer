594516ec68adbd165611054ffdc7b9544e3ffc30
sojung <sojung528@gmail.com>
2023-01-17 15:50:09 +0900

    더미 데이터 세팅 및 table underscore to java camel case 세팅 완료

---
+++ src/main/resources/db/table.sql
@@ -0,0 +1,24 @@
+create table user_tb(
+    id int auto_increment primary key,
+    username varchar not null unique,
+    password varchar not null, 
+    email varchar not null,
+    created_at timestamp
+);
+
+create table product_tb(
+    id int auto_increment primary key,
+    name varchar not null unique,
+    price int not null, 
+    qty int not null,
+    created_at timestamp
+);
+
+create table purchase_tb(
+    id int auto_increment primary key,
+    user_id int ,
+    product_id int,
+    created_at timestamp,
+    constraint fk_user foreign key (user_id) references user_tb (id),
+    constraint fk_product foreign key (product_id) references product_tb (id)
+);
