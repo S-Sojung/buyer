1c9063fec0960a76e06192321bbc0c1177d7d43c
sojung <sojung528@gmail.com>
2023-01-17 16:49:21 +0900

    레파지토리 설계 완료

---
+++ src/main/java/shop/mtcoding/buyer/model/UserRepository.java
@@ -0,0 +1,35 @@
+package shop.mtcoding.buyer.model;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+/*
+ * 회원가입, 로그인, 회원탈퇴, 회원수정
+ */
+@Mapper
+public interface UserRepository { // 만약 무슨 기능을 만들어야할 지 모르겠다면?? CRUD만 만들어주면 된다.
+    public int insert(@Param("username") String username, @Param("password") String password,
+            @Param("email") String email);
+
+    public List<User> findAll();
+
+    public User findById(int id);
+
+    public int updateById(int id, String password);
+
+    public int deleteById(int id);
+    // public int insert(@Param("username") String username, @Param("password")
+    // String password,
+    // @Param("email") String email);
+
+    // public User login(@Param("username") String username, @Param("password")
+    // String password);
+    // // findByUsernameAndPassword
+
+    // public int delete(@Param("id") int id);
+
+    // public int updatePassword(@Param("id") int id, @Param("password") String
+    // password);
+}
